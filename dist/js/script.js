47e7ec49e1132d82deac9e36ff68e48eebf6c95d
Mathieu <mathecool22@gmail.com>
2022-02-07 20:19:28 -0500

    hamburger

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,17 @@
+function functionHam() {
+    var x = document.getElementById("myLinks");
 
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJzY3JpcHQuanMiXSwic291cmNlc0NvbnRlbnQiOlsiIl0sImZpbGUiOiJzY3JpcHQuanMifQ==
 
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJzY3JpcHQuanMiXSwic291cmNlc0NvbnRlbnQiOlsiXG4vLyMgc291cmNlTWFwcGluZ1VSTD1kYXRhOmFwcGxpY2F0aW9uL2pzb247Y2hhcnNldD11dGY4O2Jhc2U2NCxleUoyWlhKemFXOXVJam96TENKdVlXMWxjeUk2VzEwc0ltMWhjSEJwYm1keklqb2lJaXdpYzI5MWNtTmxjeUk2V3lKelkzSnBjSFF1YW5NaVhTd2ljMjkxY21ObGMwTnZiblJsYm5RaU9sc2lJbDBzSW1acGJHVWlPaUp6WTNKcGNIUXVhbk1pZlE9PVxuIl0sImZpbGUiOiJzY3JpcHQuanMifQ==
+//ici on fait disparaitre le menu
+    if (x.classList.contains("ham-classe")) {
+        x.classList.remove("ham-classe");
+        x.classList.add("no-display");
+    } else {
+        //ici on fait afficher le menu
+        if (x.classList.contains("no-display")) {
+            x.classList.remove("no-display");
+        }
+        x.classList.add("ham-classe");
+
+    }
+}
